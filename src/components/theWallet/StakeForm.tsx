2bead52092fccb92413ef04a3edff8b9288955e2
tchiunam <tchiunam@users.noreply.github.com>
2022-04-13 00:02:53 +0800

    Make it stake

--- src/components/theWallet/StakeForm.tsx
+++ src/components/theWallet/StakeForm.tsx
@@ -1,8 +1,10 @@
 import { formatUnits } from "@ethersproject/units"
+import { Button, Input } from "@material-ui/core"
 import { useEthers, useTokenBalance } from "@usedapp/core"
+import { utils } from "ethers"
+import React, { useState } from "react"
+import { useStakeTokens } from "../../hooks"
 import { Token } from "../Main"
-import { Button } from "@material-ui/core"
-
 export interface StakeFormProps {
     token: Token
 }
@@ -13,9 +15,26 @@ export const StakeForm = ({ token }: StakeFormProps) => {
     const tokenBalance = useTokenBalance(tokenAddress, account)
     const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
 
+    const [amount, setAmount] = useState<number | string | Array<number | string>>(0)
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newAmount = event.target.value === "" ? "" : Number(event.target.value)
+        setAmount(newAmount)
+    }
+
+    const { approveAndStake, state: approveAndStakeErc20Send } = useStakeTokens(tokenAddress)
+
+    const handleStakeSubmit = () => {
+        const amountAsWei = utils.parseEther(amount.toString())
+        return approveAndStake(amountAsWei.toString())
+    }
     return (
         <>
-            <Button color="primary" size="large"> Stake Now </Button>
+            <Input
+                onChange={handleInputChange} />
+            <Button
+                onClick={handleStakeSubmit}
+                color="primary"
+                size="large">Stake Now</Button>
         </>
     )
 }
